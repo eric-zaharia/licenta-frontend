689c52e4d5da68947c5727d43e27f6a7c47e3ebd
eric-zaharia <zahariaeric@gmail.com>
2025-04-01 14:12:27 +0300

    added expand menu for each password of user

--- src/app/pages/my-passwords/my-passwords.component.ts
+++ src/app/pages/my-passwords/my-passwords.component.ts
@@ -1,19 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { PasswordService } from '../../services/password/password.service';
+import { MatList, MatListItem } from '@angular/material/list';
+import {
+    MatExpansionPanel,
+    MatExpansionPanelDescription,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle
+} from '@angular/material/expansion';
 
 @Component({
     selector: 'app-my-passwords',
-    imports: [],
+    imports: [
+        MatList,
+        MatListItem,
+        MatExpansionPanel,
+        MatExpansionPanelHeader,
+        MatExpansionPanelTitle,
+        MatExpansionPanelDescription
+    ],
     templateUrl: './my-passwords.component.html',
     styleUrl: './my-passwords.component.css'
 })
 export class MyPasswordsComponent implements OnInit {
     passwordService: PasswordService = inject(PasswordService);
-    passwords: Object = [];
+    passwords: any[] = [];
+    panelOpenState: any[] = [];
 
     ngOnInit() {
-        this.passwordService.getAllUserPasswords().subscribe((passwords) => {
+        this.passwordService.getAllUserPasswords().subscribe((passwords: any) => {
             this.passwords = passwords;
+            for (let i = 0; i < passwords.length; i++) {
+                this.panelOpenState.push(signal(false));
+            }
             console.log(passwords);
         })
     }
