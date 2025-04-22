7e2f19dcb25b3af3bf7c2ad19f86677a63377283
eric-zaharia <zahariaeric@gmail.com>
2025-04-22 15:18:13 +0300

    added card to home page with available pages to navigate to (not clickable yet)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { MatIcon } from "@angular/material/icon";
+import { MatLine } from "@angular/material/core";
+import { MatList, MatListItem, MatNavList } from "@angular/material/list";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+    imports: [
+        MatIcon,
+        MatLine,
+        MatList,
+        MatListItem,
+        RouterLink,
+        RouterLinkActive,
+        MatCard,
+        MatCardContent,
+        MatCardHeader,
+        MatCardTitle
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    authService: AuthService = inject(AuthService);
+    authenticated = false;
+
+    ngOnInit() {
+        this.authService.authStatus$.subscribe(authStatus => {
+            this.authenticated = authStatus;
+        })
+    }
 }
