64f1a1f2f89043e7d4c8309bacf73fb05186c73f
eric-zaharia <zahariaeric@gmail.com>
2025-05-01 10:54:55 +0300

    modified the home page to include small description and usage recommendations.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
-import { MatIcon } from "@angular/material/icon";
-import { MatLine } from "@angular/material/core";
-import { MatList, MatListItem, MatNavList } from "@angular/material/list";
-import { RouterLink, RouterLinkActive } from "@angular/router";
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
     imports: [
-        MatIcon,
-        MatLine,
-        MatList,
-        MatListItem,
-        RouterLink,
-        RouterLinkActive,
         MatCard,
         MatCardContent,
         MatCardHeader,
-        MatCardTitle
+        MatCardTitle,
+        MatDivider,
+        RouterLink
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
